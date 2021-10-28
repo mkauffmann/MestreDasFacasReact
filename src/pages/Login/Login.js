764a4b35e6bc0032e526ed3d286ce22851a2454f
Maria Kauffmann <70024598+mkauffmann@users.noreply.github.com>
2021-10-28 18:23:35 -0300

    component btn principal, btn cancelar e btn link

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Login.css'
 
+import Button from '../../components/micro/Button/Button'
+
 function Login(props) {
 
     return(
         <>
-        <div>Login</div>
+        <Button navigation route='home' label='principal' class='btn-cancelar'/>
+        <Button label='principal' class='btn-principal'/>
+        <Button label='cancelar' class='btn-cancelar'/>
         </>
     )
 }
