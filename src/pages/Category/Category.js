2d0e975e0bbe4b9a4bffed8ec0147302b7457227
JVCanada <jv.canada1@gmail.com>
2021-10-29 12:18:12 -0300

    atualização de página de produto e catálogo

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -5,7 +5,270 @@ function Category(props) {
 
     return(
         <>
-        <div>Category</div>
+
+        {/* <!-- BEGIN FILTROS --> */}
+        
+        <div class="container mt-5 mb-4">
+                <div class="row">
+                    <h2 class="texto-pesquisa mb-4"> Você buscou por X </h2>
+                </div>
+                {/* <div class="row">
+                    <div class="col-12 col-md-6 col-lg-3 filtros">
+                        <h5 class="titulo-filtro mt-3"> Desconto </h5>
+                        <div class="form-floating mb-3">
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control caixa-filtro" placeholder="Busque por desconto" aria-label="filtro" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="desconto">
+                            <label class="form-check-label label-check-custom" for="desconto"> 6% a 8% </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="desconto">
+                            <label class="form-check-label label-check-custom" for="desconto"> 9% a 15% </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="desconto">
+                            <label class="form-check-label label-check-custom" for="desconto"> 16% a 28% </label>
+                        </div>
+
+                        <hr>
+
+                        <h5 class="titulo-filtro"> Marca </h5>
+                        <div class="form-floating mb-3">
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control caixa-filtro" placeholder="Busque por marca" aria-label="filtro" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="marca">
+                            <label class="form-check-label label-check-custom" for="marca"> X </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="marca">
+                            <label class="form-check-label label-check-custom" for="marca"> Y </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="marca">
+                            <label class="form-check-label label-check-custom" for="marca"> Z </label>
+                        </div>
+
+                        <hr>
+
+                        <h5 class="titulo-filtro"> Sub-Categorias </h5>
+                        <div class="form-floating mb-3">
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control caixa-filtro" placeholder="Busque por sub-categoria" aria-label="filtro" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="sub-categoria">
+                            <label class="form-check-label label-check-custom" for="sub-categoria"> Chefe </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="sub-categoria">
+                            <label class="form-check-label label-check-custom" for="sub-categoria"> Legumes </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="sub-categoria">
+                            <label class="form-check-label label-check-custom" for="sub-categoria"> Pão </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="sub-categoria">
+                            <label class="form-check-label label-check-custom" for="sub-categoria"> Cutelo </label>
+                        </div>
+
+                        <hr>
+
+                        <h5 class="titulo-filtro"> Preço </h5>
+                        <div class="form-floating mb-3">
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control caixa-filtro" placeholder="Busque por preço" aria-label="filtro" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="preco">
+                            <label class="form-check-label label-check-custom" for="preco"> R$ 30 a R$ 60 </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="preco">
+                            <label class="form-check-label label-check-custom" for="preco"> R$ 60 a R$ 120 </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="preco">
+                            <label class="form-check-label label-check-custom" for="preco"> R$ 120 a R$ 200 </label>
+                        </div>
+                        <div class="mb-1 form-check">
+                            <input type="checkbox" class="form-check-input custom-check" id="preco">
+                            <label class="form-check-label label-check-custom" for="preco"> R$ 200 a R$ 20000 </label>
+                        </div>
+
+                        <hr> */}
+
+                {/* <!-- ENDING FILTROS --> */}
+
+                {/* <!-- BEGIN CATALOGO PRODUTOS --> */}
+
+                {/* </div> */}
+                    <div class="col-12 col-md-12 col-lg-12 catalogo-produtos">
+                        <div class="row">
+                            <div class="col-12 col-md-6 col-lg-12 linha-produtos-encontrados"> 
+                                <h5 class="produtos-encontrados"> X Produtos Encontrados </h5>
+                                <div class="dropdown drop-ordena">
+                                    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Ordenar por:
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#"> Relevância </a></li>
+                                    <li><a class="dropdown-item" href="#"> Maior Preço </a></li>
+                                    <li><a class="dropdown-item" href="#"> Menor Preço </a></li>
+                                    <li><a class="dropdown-item" href="#"> Mais Vendido </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row catalogo-produtos2">
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'" > Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                        </div>
+
+                        <div class="row catalogo-produtos2">
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                        </div>
+
+                        <div class="row catalogo-produtos2">
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                        </div>
+
+                        <div class="row catalogo-produtos2">
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 item-catalogo">
+                                <label class="imagem-produto"> IMAGEM PRODUTO </label>
+                                {/* <img src="./img/Classificacao_Estrelas.png" class="classifica-estrelas" width="100px"> */}
+                                <p class="nome-do-produto"> Nome do produto </p>
+                                <p class="preco-do-produto"> R$ XX,XX </p> 
+                                <button type="submit" class="btn-principal btn-comprar" onclick="window.location.href='pdp.html'"> Comprar </button>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    {/* <!-- ENDING CATALOGO PRODUTOS --> */}
+
+                    {/* <!-- BEGIN BOTAO CARREGAR --> */}
+
+                    <div class="row linha-botao-carregar">
+                        <div class="col-12 col-md-6 col-lg-3 botao-carregar">
+                            <br/><br/><br/><br/>
+                            <button type="reset" class="btn-cancelar"> Carregar mais </button>
+                        </div>
+                    </div>
+
+                    {/* <!-- ENDING BOTAO CARREGAR --> */}
+
+            </div>
+
         </>
     )
 }
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,11 +1,114 @@
 import React from 'react'
 import './Product.css'
 
+import Button from '../../components/micro/Button/Button'
+import '../../components/micro/Button/Button.css'
+
 function Product(props) {
 
     return(
         <>
-        <div>Product</div>
+        <div className="container mt-5 mb-4 produto">
+                <div className="row">
+                    <p className="texto-categoria"> Mestre das Facas - Categoria 1 </p>
+                    <div className="col-12 col-md-6 col-lg-3 col-produto">
+                        <label className="imagem-produto"> IMAGEM PRODUTO </label>
+                        {/* Classificação e calculo de frete: */}
+                        {/* <img src="./img/Classificacao_Estrelas.png" className="classifica-estrelas" width="100px"/> */}
+                            {/* <div className="col-12 col-md-6 col-lg-10">
+                                <p> Calcular o frete e prazo de entrega </p>
+                                <div className="input-group input-group-sm mb-3">
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="_____-___"/> */}
+                                    {/* <button type="reset" className="btn-cancelar mx-2"> Calcular </button> */}
+                                    {/* <Button label='Calcular' class='btn-cancelar'/>
+                                </div>
+                            </div> */}
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-9">
+                        <div className="nome-do-produto">
+                           <p> Nome do produto </p> 
+                        </div>
+                        <div className="row">
+                            <div className="col-12 col-md-6 col-lg-5">
+                                <select className="form-select caixa-cor-cabo" aria-label="Default select example">
+                                    <option selected>Cor do cabo</option>
+                                    <option value="1">Azul</option>
+                                    <option value="2">Verde</option>
+                                    <option value="3">Amarelo</option>
+                                  </select>
+                            </div>
+                            <div className="col-12 col-md-6 col-lg-5">
+                                    {/* <button type="submit" className="btn-principal" onclick="window.location.href='meucarrinho.html'"> Comprar </button> */}
+                                    <Button label='Comprar' class='btn-principal btn-comprar'/>
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className="preco"> R$ XX,XX </h3>
+                        </div>
+                        <p className="texto-desconto"> 10% de desconto em 1x no Cartão ou Boleto. sem juros </p>
+                        <hr/>
+                        <p className="texto-cartao-credito"> R$ XX,XX ou até 1x de R$XX,XX sem juros no Cartão de Crédito </p>
+                        <hr/>
+                        <p className="texto-boleto"> R$ XX,XX - 10% de desconto
+                        em 1x no Boleto. </p>
+                        <hr/>
+                        <p className="texto-pix"> R$ XX,XX - Em pagamento por PIX </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* <!-- ENDING PRODUTO --> */}
+
+            {/* <!-- BEGIN DESCRICAO --> */}
+
+            <div className="container mt-5 mb-4 produto">
+
+
+                <div className="row">
+                    <div className="col-12 col-md-12 col-lg-12 col-descricao">
+                        <h3 className="mt-2"> Descrição do Produto </h3>
+                        <p> Morbi nisi diam, aliquet ut vulputate sit amet, sodales ut metus. Sed ut nisl consectetur, molestie neque nec, mollis velit. Sed sit amet erat dolor. Ut quis ligula eu neque egestas faucibus. Sed eleifend arcu ac quam cursus, sed dignissim magna feugiat. Nullam eget tortor id est interdum accumsan. Fusce volutpat, nisi ut pharetra sodales, lacus odio interdum purus, sed dignissim leo ipsum ac lectus. </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-12 col-lg-12 col-especificacoes mt-5">
+                        <h3 className="mt-2"> Especificações e Dimensões </h3>
+                        <ul>
+                            <li> Altura:  </li>
+                            <li> Largura:  </li>  
+                            <li> Comprimento:  </li>  
+                            <li> Peso: </li> 
+                        </ul>
+                    </div>
+                </div>
+
+            {/* <!-- ENDING DESCRICAO --> */}
+
+            {/* <!-- BEGIN AVALIACAO --> */}
+            
+
+                {/* <div class="row">
+                    <div class="col-12 col-md-6 col-lg-12 mt-4">
+                        <h3 class="texto-avalie-produto"> Avalie o Produto: </h3>
+                    </div>
+                </div> */}
+
+                {/* <div class="row">
+                    <div class="col-12 col-md-6 col-lg-12 mt-4">
+                        <h3 class="mt-2"> Deixe um comentário! </h3>
+                        <div class="input-group">
+                            <textarea class="form-control" aria-label="With textarea"></textarea>
+                        </div>
+                    </div>
+                </div> */}
+
+                {/* <div class="row">
+                    <div class="col-12 col-md-6 col-lg-3 mt-4">
+                        <button type="reset" class="btn-cancelar btn-enviar"> Enviar </button>
+                    </div>
+                </div> */}
+            </div>
+
         </>
     )
 }
