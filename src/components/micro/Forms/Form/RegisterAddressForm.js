0cf5b9da8e68499999ca06e60ed8f8f088caa100
Maria Kauffmann <70024598+mkauffmann@users.noreply.github.com>
2021-11-08 11:23:11 -0300

    componente de formulario de endereco com validacao

--- src/components/micro/Forms/Form/RegisterAddressForm.js
+++ src/components/micro/Forms/Form/RegisterAddressForm.js
@@ -2,33 +2,37 @@ import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import Input from "../Input/Input";
 import Button from "../../Button/Button";
+import useValidation from "../../../../hooks/useValidation";
 
 const initialValue = {
     street: "",
     number: "",
+    complement: "",
     cep: "",
     neighborhood: "",
-    city:
-    {
-        cityName: ""
-    },
-    state:
-    {
-        uf: ""
-    }
+    cityTemp: "",
+    stateTemp: ""
 }
 
 function RegisterAddressForm(props) {
     const [addressValues, setAddressValues] = useState({ ...initialValue })
-    const [errors, setErrors] = useState({})
+    const requiredFields = ["street", "number", "cep", "neighborhood", "cityTemp", "stateTemp"]
+    const {
+        validateForm,
+        resetErrorStates,
+        errors,
+        validateStringNotEmpty,
+        validateNotRequired,
+        isEmpty
+    } = useValidation(addressValues);
+
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        if (validateForm()) {
+        if (validateForm(requiredFields)) {
             props.save(addressValues);
             setAddressValues({ ...initialValue });
-            setErrors({});
         }
     }
 
@@ -43,31 +47,83 @@ function RegisterAddressForm(props) {
 
     const resetForm = () => {
         setAddressValues({ ...initialValue })
-        setErrors({})
+        resetErrorStates()
     }
 
-    const validateForm = () => {
-        let isValid = true;
-
-        return isValid;
+    const handleBlur = (event, validationCallback) => {
+        const value = event.target.value;
+        const name = event.target.name;
+        validationCallback(value, name)
     }
+
 
     return (
         <form onSubmit={handleSubmit} onReset={resetForm}>
-            <Col>
+            <Col md={10} lg={8} className="mx-auto">
                 <Row>
-                    <Col>
+                    <Col md={6} className="mb-3">
+                        <Input type="text" id="cep" name="cep"
+                        mask="99999-999"
+                        label="CEP" obrigatorio
+                        placeholder="Digite seu CEP"
+                        changeFunction={handleChange} value={addressValues.cep}
+                        blurFunction={handleBlur} validation={isEmpty}
+                        error={errors.cep} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={10} className="mb-3">
                         <Input type="text" id="street" name="street"
                             label="Logradouro" obrigatorio
                             placeholder="Rua, Avenida, etc."
                             changeFunction={handleChange} value={addressValues.street}
+                            blurFunction={handleBlur} validation={validateStringNotEmpty}
                             error={errors.street} />
+                    </Col>
+                    <Col md={2} className="mb-3">
+                        <Input type="text" id="number" name="number"
+                                label="Número" obrigatorio
+                                mask="999999" changeFunction={handleChange} value={addressValues.number}
+                                blurFunction={handleBlur} validation={isEmpty}
+                                error={errors.number} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="mb-3">
+                        <Input type="text" id="complement" name="complement"
+                                label="Complemento" placeholder="Apartamento, Bloco, Casa (opcional)"
+                                changeFunction={handleChange} value={addressValues.complement}
+                                blurFunction={handleBlur} validation={validateNotRequired}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={5} className="mb-3">
+                        <Input type="text" id="neighborhood" name="neighborhood"
+                                label="Bairro" obrigatorio 
+                                changeFunction={handleChange} value={addressValues.neighborhood}
+                                blurFunction={handleBlur} validation={validateStringNotEmpty}
+                                error={errors.neighborhood}/>
+                    </Col>
+                    <Col md={5} className="mb-3">
+                        <Input type="text" id="city" name="cityTemp"
+                                label="Cidade" obrigatorio
+                                changeFunction={handleChange} value={addressValues.cityTemp}
+                                blurFunction={handleBlur} validation={validateStringNotEmpty}
+                                error={errors.cityTemp} />
+                    </Col>
+                    <Col md={2} className="mb-3">
+                        <Input type="text" id="state" name="stateTemp"
+                                label="UF" obrigatorio
+                                mask="aa"
+                                changeFunction={handleChange} value={addressValues.stateTemp}
+                                blurFunction={handleBlur} validation={isEmpty}
+                                error={errors.stateTemp} />
                     </Col>
                 </Row>
                 <Row className="my-3 justify-content-end">
                     <Col className="d-flex justify-content-end">
                         <Button class="btn-cancelar mx-2" label="Limpar" type="reset" />
-                        <Button class="btn-principal" label="Cadastrar" type="submit" />
+                        <Button class="btn-principal" label="Salvar" type="submit" />
                     </Col>
                 </Row>
             </Col>
