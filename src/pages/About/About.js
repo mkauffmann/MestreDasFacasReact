ddc5b9650683aed462df8693e2a4c4fbffd22ebb
JVCanada <jv.canada1@gmail.com>
2021-11-10 11:51:59 -0300

    alterações nas páginas de categoria e criação de quem somos

---
+++ src/pages/About/About.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import '../About/About.css'
+
+function About(props) {
+
+    return (
+        <>
+            <div className="container mt-5 mb-4"> 
+
+                <div className="row"> 
+                    <h2 className="mb-4"> Quem somos </h2>
+                </div> 
+                <div className="row">
+                    <p className="descricao-quem-somos"> 
+                        Nós somos o mestre das facas e vendemos facas.
+                    </p>
+                </div>
+
+                <div className="row"> 
+                    <h2 className="mb-4"> Missão </h2>
+                </div> 
+                <div className="row">
+                    <p className="descricao-quem-somos"> 
+                        Lorem ipsum dolor sit amet, consectetur adipis.
+                    </p>
+                </div>
+
+                <div className="row"> 
+                    <h2 className="mb-4"> Visão </h2>
+                </div> 
+                <div className="row">
+                    <p className="descricao-quem-somos"> 
+                        Lorem ipsum dolor sit amet, consectetur adipis.
+                    </p>
+                </div>
+
+                <div className="row"> 
+                    <h2 className="mb-4"> Valores </h2>
+                </div> 
+                <div className="row">
+                    <p className="descricao-quem-somos"> 
+                        Lorem ipsum dolor sit amet, consectetur adipis.
+                    </p>
+                </div>
+
+                
+            
+            
+            </div>
+        </>
+    )
+
+
+}
+
+export default About;
