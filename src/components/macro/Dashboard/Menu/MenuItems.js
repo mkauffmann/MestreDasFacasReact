24a976322b3524bacf629ffd86a830b103c2e2db
Maria Kauffmann <70024598+mkauffmann@users.noreply.github.com>
2021-11-10 17:19:29 -0300

    ajustes menu mobile dashboard

--- src/components/macro/Dashboard/Menu/MenuItems.js
+++ src/components/macro/Dashboard/Menu/MenuItems.js
@@ -9,12 +9,12 @@ function MenuItems(props) {
 
     return (
         <ul className="menu-items">
-            <li className="menu-item" onClick={() => props.toggleMobileMenu()}><Link to={`${url}/userForm`}>Meus Dados</Link></li>
-            <li className="menu-item" onClick={() => props.toggleMobileMenu()}><Link to={`${url}/myOrders`}>Meus Pedidos</Link></li>
-            <li className="menu-item" onClick={() => props.toggleMobileMenu()}><Link to={`${url}/myCards`}>Meus Cartões</Link></li>
-            <li className="menu-item" onClick={() => props.toggleMobileMenu()}><Link to={`${url}/myAddresses`}>Meus Endereços</Link></li>
-            <li className="menu-item" onClick={() => props.toggleMobileMenu()}><Link to={`${url}/myTelephones`}>Meus Telefones</Link></li>
-            <li className="menu-item" onClick={() => props.toggleMobileMenu()}><a href="">Sair</a></li>
+            <li className="menu-item" onClick={() => props.toggleMobileMenu && props.toggleMobileMenu()}><Link to={`${url}/userForm`}>Meus Dados</Link></li>
+            <li className="menu-item" onClick={() => props.toggleMobileMenu && props.toggleMobileMenu()}><Link to={`${url}/myOrders`}>Meus Pedidos</Link></li>
+            <li className="menu-item" onClick={() => props.toggleMobileMenu && props.toggleMobileMenu()}><Link to={`${url}/myCards`}>Meus Cartões</Link></li>
+            <li className="menu-item" onClick={() => props.toggleMobileMenu && props.toggleMobileMenu()}><Link to={`${url}/myAddresses`}>Meus Endereços</Link></li>
+            <li className="menu-item" onClick={() => props.toggleMobileMenu && props.toggleMobileMenu()}><Link to={`${url}/myTelephones`}>Meus Telefones</Link></li>
+            <li className="menu-item" onClick={() => props.toggleMobileMenu && props.toggleMobileMenu()}><a href="">Sair</a></li>
         </ul>
     )
 }
